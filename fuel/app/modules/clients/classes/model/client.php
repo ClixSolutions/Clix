b4a17efa7ddf23821560f73374265828ae346c8d
Simon Skinner <codewith@vultuk.me>
2013-09-20 15:30:07 +0100

    Client Migrations Done

---
+++ fuel/app/modules/clients/classes/model/client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Clients;
+
+class Model_Client
+{
+
+    public static function Create_Client()
+    {
+
+    }
+
+    public static function Set_Status($id, $status)
+    {
+
+    }
+
+}
