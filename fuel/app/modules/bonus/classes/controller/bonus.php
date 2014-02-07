37d704c92c743a55da56891b89a9b3eb4a6768f4
Simon Skinner <codewith@vultuk.me>
2014-02-07 10:43:32 +0000

    Added test controller

--- fuel/app/modules/bonus/classes/controller/bonus.php
+++ fuel/app/modules/bonus/classes/controller/bonus.php
@@ -10,4 +10,10 @@ class Controller_Bonus extends \Clients\Controller_Force
 
     }
 
+    public function action_test()
+    {
+        $bonus = new Bonus();
+        $bonus->investment(100,100,0,'Starting Balance');
+    }
+
 }
